295f6d824b15824a4bdec266fca99a7697f8dd5e
Eathyn <eathynzakas@gmail.com>
2022-01-20 07:07:17 +0800

    [feat] add tabs and categories

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,28 @@
 Page({
-  data: {},
+  data: {
+    tabs: ['全部服务', '在提供', '正在找'],
+    currentTabIndex: 0,
+    categoryList: [
+      { id: 1, name: '保洁' },
+      { id: 2, name: '汽修' },
+      { id: 3, name: '疏通' },
+    ]
+  },
+
   onLoad: function (options) {
 
-  }
+  },
+
+  handleTabChange(e) {
+    const { index } = e.currentTarget.dataset
+    this.setData({
+      currentTabIndex: index,
+    })
+  },
+
+  handleCategoryChange(e) {
+    const { id } = e.currentTarget.dataset
+  },
+
+
 });
