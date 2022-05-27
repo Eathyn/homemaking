5ab82b0739893a75af51d8ae4db37fe710ea0d17
Eathyn <eathynzakas@gmail.com>
2022-05-27 20:54:57 +0800

    3.3-3.4: 广场页面标签页效果

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,8 +1,16 @@
 Page({
-    data: {
+  data: {
+    tabs: ['全部服务', '在提供', '正在找'],
+    currentTabIndex: 0,
+  },
+  onLoad: function (options) {
 
-    },
-    onLoad: function (options) {
+  },
 
-    }
+  handleTabChange(evt) {
+    const { index } = evt.currentTarget.dataset
+    this.setData({
+      currentTabIndex: index,
+    })
+  }
 });
