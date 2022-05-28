62f183f48145ade75ccde23854281d622eb03182
Eathyn <eathynzakas@gmail.com>
2022-05-28 11:41:38 +0800

    3.5: 分类筛选功能实现
    swiper, swiper-item

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,6 +2,20 @@ Page({
   data: {
     tabs: ['全部服务', '在提供', '正在找'],
     currentTabIndex: 0,
+    categoryList: [
+      {
+        id: 1,
+        name: '保洁',
+      },
+      {
+        id: 2,
+        name: '汽修',
+      },
+      {
+        id: 3,
+        name: '疏通',
+      },
+    ],
   },
   onLoad: function (options) {
 
@@ -12,5 +26,9 @@ Page({
     this.setData({
       currentTabIndex: index,
     })
+  },
+
+  handleCategoryChange(evt) {
+    const { id } = evt.currentTarget.dataset
   }
 });
