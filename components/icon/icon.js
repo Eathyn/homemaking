6bf0e11e9e6f331f848b87af52a468549c2a0932
Eathyn <eathynzakas@gmail.com>
2022-05-31 12:21:24 +0800

    5.9 - 6.3
    封装icon组件

---
+++ components/icon/icon.js
@@ -0,0 +1,17 @@
+Component({
+  properties: {
+    name: {
+      type: String,
+    },
+    color: {
+      type: String,
+      value: '#FBC02D'
+    },
+    size: {
+      type: String,
+      value: '32',
+    },
+  },
+  data: {},
+  methods: {},
+})
