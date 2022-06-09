a39d0eea807b97e001236e7761c9d0827744122a
Eathyn <eathynzakas@gmail.com>
2022-06-09 13:46:36 +0800

    feat: 封装头像组件

---
+++ components/avatar/avatar.js
@@ -0,0 +1,28 @@
+Component({
+  properties: {
+    src: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+    shape: {
+      type: String,
+      value: 'square', // 'square' / 'circle'
+    },
+    radius: {
+      type: String,
+      value: '0'
+    },
+    size: {
+      type: String,
+      value: '120'
+    },
+    fontSize: {
+      type: String,
+      value: '28'
+    },
+  },
+  data: {},
+  methods: {},
+})
