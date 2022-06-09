fbed2fdb5ca9374ba2a4d506c5b7ae5db2a1854e
Eathyn <eathynzakas@gmail.com>
2022-06-09 13:58:03 +0800

    feat: 服务行为/状态/类型枚举值

---
+++ enum/service-action.js
@@ -0,0 +1,7 @@
+const serviceAction = {
+  PUBLISH: 1, // 发布服务
+  PAUSE: 2, // 暂停发布
+  CANCEL: 3, // 取消服务
+}
+
+export default serviceAction
---
+++ enum/service-status.js
@@ -0,0 +1,10 @@
+const serviceStatus = {
+  PENDING: 0, //待审核
+  UNPUBLISHED: 1, // 待发布
+  PUBLISHED: 2, // 已发布
+  OFF_SHELVES: 3, // 已下架
+  CANCELED: 4, // 已取消
+  DENY: 5, // 审核不通过
+}
+
+export default serviceStatus
