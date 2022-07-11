e7cbe70cf95149f6bc23f509374ff53a73ab715c
Eathyn <eathynzakas@gmail.com>
2022-07-11 12:07:56 +0800

    10-2

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,11 +1,31 @@
 import cache from '../../enum/cache'
 import { setTabBarBadge } from '../../utils/wx'
+import Token from '../../model/token'
+import User from '../../model/user'
 
 Page({
-  data: {},
-
-  onShow() {
-    const unreadCount = wx.getStorageSync(cache.UNREAD_COUNT)
-    setTabBarBadge(unreadCount)
+  data: {
+    userInfo: {
+      nickname: '点击授权登陆',
+      avatar: '../../images/logo.png',
+    },
   },
-});
+
+  async onShow() {
+    const unreadCount = wx.getStorageSync(cache.UNREAD_COUNT)
+    await setTabBarBadge(unreadCount)
+    const verifyToken = await Token.verifyToken()
+    if (verifyToken.valid) {
+      const userInfo = User.getUserInfoByLocal()
+      this.setData({
+        userInfo,
+      })
+    }
+  },
+
+  handleToLogin() {
+    wx.navigateTo({
+      url: '/pages/login/login',
+    })  
+  },
+})
