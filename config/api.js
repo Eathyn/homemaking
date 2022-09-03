7cc1c55c7274772d02e643ae19fcc866c8b6a260
Eathyn <eathynzakas@gmail.com>
2022-09-03 12:04:25 +0800

    chore: delete iCode and orderNo

--- config/api.js
+++ config/api.js
@@ -1,7 +1,7 @@
 const ApiConfig = {
   baseUrl: 'https://qinchenju.com/homemaking/',
-  iCode: '00134747A1AA0327',
-  orderNo: '2201042244416185',
+  iCode: '',
+  orderNo: '',
 }
 
 export default ApiConfig
